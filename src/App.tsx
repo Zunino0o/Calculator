bebc4d603bd0076526fa5e89a057d416d68e0acc
Lucas Zunino <zunino10@hotmail.com>
2024-02-21 22:09:44 -0300

    voltamos/adiciona botoes numericos e operacionais, resultado e operacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,57 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [first, setFirst] = useState('');
+  const [second, setSecond] = useState('');
+  const [operation, setOperation] = useState('+');
+  const [operationChosen, setOperationChosen] = useState(false);
+  const [result, setResult] = useState('');
+
+  const handleOperation = ({ target }) => {
+    setOperation(target.value);
+    setOperationChosen(true);
+  };
+  const handleNumbers = ({ target }) =>
+    operationChosen
+      ? setSecond(second + target.value)
+      : setFirst(first + target.value);
+  const handleResult = () => {
+    setResult(eval(`${first || 0} ${operation} ${second || 0}`));
+  };
+  const handleClear = () => {
+    setFirst('');
+    setSecond('');
+    setOperation('+');
+    setOperationChosen(false);
+    setResult('');
+  };
+
   return (
-    <h1>Hello World!</h1>
-  )
+    <div>
+      <h2>
+        OPERATION: {first.length ? first : 0} {operation}{' '}
+        {second.length ? second : 0}
+      </h2>
+      <h1>RESULT: {result}</h1>
+      <div>
+        {Array.from({ length: 10 }, (_, i) => (
+          <button key={i} value={i} onClick={handleNumbers}>
+            {i}
+          </button>
+        ))}
+      </div>
+      {['+', '-', '*', '/'].map((op) => (
+        <button key={op} value={op} onClick={handleOperation}>
+          {op}
+        </button>
+      ))}
+      <button type="submit" onClick={handleResult}>
+        SUBMIT
+      </button>
+      <button type="submit" onClick={handleClear}>
+        CLEAR
+      </button>
+    </div>
+  );
 }
-export default App
+export default App;
